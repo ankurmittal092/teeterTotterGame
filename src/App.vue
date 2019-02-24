333c1de9543ad063f7615f6465a4505754443e0b
Ankur Mittal <ankurmittal092@gmail.com>
2019-02-24 12:03:55 +0530

    Vuex store init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TeeterTotterGame msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TeeterTotterGame from './modules/teeter-totter-game'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TeeterTotterGame
   }
 }
 </script>
---
+++ src/modules/teeter-totter-game/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div v-on:click="greet">{{ test }}</div>
+    <input v-model="messageData" />
+    <div>Yooo - {{ testTodo }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TeeterTotterGame',
+  props: {
+    msg: String
+  },
+  data: function () {
+    return {
+      test: 'hello hello',
+      messageData: this.msg
+    };
+  },
+  methods: {
+    greet: function (params) {
+      console.log(this, this.$store);
+    },
+    inputChange: console.log
+  },
+  computed: {
+    testTodo: function () {
+      return this.$store.getters.testTodo;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
